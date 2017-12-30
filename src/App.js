36b9a452358289eda36aaa109cc93c4920ddd619
jeffkealy <jeffkealy@yahoo.com>
2017-12-29 18:41:49 -0600

    need to get all data to card

--- src/App.js
+++ src/App.js
@@ -3,17 +3,58 @@ import {Wrapper} from './components/ReactSwipeCard.js'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    // this code runs first
+    super(props)
+    this.state= {
+      restaurantInfo : [
+        {
+          name: 'Lowbrow',
+          dish: 'Burger'
+        },
+        {
+          name: "Pat's Tap",
+          dish: 'chicken Sandwich'
+        },
+        {
+          name: "The Lowry",
+          dish: 'Chilaquiles'
+        }
+      ],
+      index: 0
+    }
+    // this.getData()
+  }
+  // getData() {
+  //   fetch(YOUR_DATABASE).then(res => this.setState({restaurantInfo: res.data}))
+  // }
+  nextCard(props, i) {
+    console.log('nextCard', props);
+    console.log('index', i);
+
+    // this.setState({index: this.state.index + 1})
+
+  }
   render() {
+    // if (this.state.restaurantInfo[0]) {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Füdr</h1>
         </header>
         <div className="App-intro">
-          <Wrapper/>
+          <Wrapper
+            restaurantName={this.state.restaurantInfo.map(data => data.name)}
+            dishName={this.state.restaurantInfo.map(data => data.dish)}
+            nextCard={this.nextCard}
+          />
+
         </div>
       </div>
     );
+  // } else{
+  //     return null
+  //   }
   }
 }
 
--- src/components/ReactSwipeCard.js
+++ src/components/ReactSwipeCard.js
@@ -2,17 +2,22 @@ import React from 'react';
 import Cards, { Card } from 'react-swipe-card'
 import './Cards.css';
 
-
-const data = ['Jake', 'Eric', 'Alina', "Jeffy"]
-export const Wrapper = () => {
+export const Wrapper = (props) => {
+  console.log('props', props);
   return (
       <Cards onEnd={console.log('end')} alertLeft={<div>No</div>} alertRight={<div>Yes</div>} className='master-root'>
-        {data.map((item, i) =>
+        {props.restaurantName.map((name, i) =>
           <Card key={i}
-            onSwipeLeft={()=>{console.log()}}
-            onSwipeRight={()=>{console.log('swipe right', item)}}
-            >
-            <h2>{item}</h2>
+           onSwipeLeft={()=>props.nextCard(props, i)}
+           onSwipeRight={()=>props.nextCard()}
+           >
+           <div>
+            <h2>{name}</h2>
+            <h2>{props.dishName.map(data => data.dish)}</h2>
+            <h2>{i}</h2>
+
+           </div>
+
           </Card>
         )}
       </Cards>
